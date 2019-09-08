cbc291d55613db71a9c0ed17f5d96b2e08e8ee78
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-09-08 15:17:01 -0400

    added questions object.  Consoled correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,77 @@
-alert("TESTING");
+// alert("TESTING");
 
 //                     global variables
 // ------------------------------------------------------------
 
-var triviaQuestions = {
-    question: "What type of galaxy is the most common in the universe?",
-    answer: "Elliptical Gallaxies"
-};  console.log("QUESTION " + triviaQuestions.question);
+var triviaQuestions = [
+    {
+        question: "What type of galaxy is the Milky Way Galaxy?",
+        choices: ["Ellipitical", " Spiral", " Spiral Barred", " Irregular "],
+        answer: "Spiral"
+    },
+    
+    {
+        question: "What is the largest planet in our solar system?  (Fun fact - this is also my favorite planet",
+        choices: ["Earth", " Mars", " Neptune", " Jupiter"],
+        answer: "Jupiter"
+    },
+
+    {
+        question: "What is the smallest planet on our solar system?",
+        choices: ["Pluto", " Venus", " Sature", " Mercury"],
+        answer: "Mercury"
+    },
+
+    {
+        question: "What hads a gravitational pull so strong that even light cannot escape it?",
+        choices: ["quasar", " nebula", "black hole", " red giants"],
+        answer: "black hole"
+    },
+
+    {
+        questions: "What is the Earth's Moon commonly referred to? Hint - It's Latin for Moon and the name of one of my pups",
+        choices: ["Sol", " Terra", " Belatrix", " Luna"],
+        answer: "Luna"
+    },
+
+    {
+        question: "How much time does it take for the Sun's Rays to reach Earth?",
+        choices: ["24 hours", " 365 days", " 8 minutes", " 60 minutes"],
+        answer: "8 minutes"
+    },
+
+    {
+        question: "Which planet has approximately the same landmass as Earth?",
+        choices: ["Venus", " Uranus", " Mars", " Jupiter"],
+        answer: "Mars"
+    },
+
+    {
+        question: "When was Pluto reclassified from a planet to a dwarf planet? (You're still a planet to me little buddy)",
+        choices: ["2006", " 1999", " 2009", " 2012"], 
+        answer: "2006"
+    },
+
+    {
+        question: "What are the most luminous objects in the known Universe?",
+        choices: ["black holes", " stars", " quasars", " planets"],
+        answer: "quasars"
+    },
+
+    {
+        question: "What was the first planet discovered with the use of a telescope?",
+        choices: ["Uranus", " Mars", " Neptune", "Pluto"],
+        answer: "Uranus"
+    }
+]; 
+console.log(triviaQuestions);
+// console.log(triviaQuestions[1].choices[3]); output - Jupiter
+
+
+
+
+
+
 
 
 
@@ -24,7 +89,7 @@ var triviaQuestions = {
 // What is the smallest planet in our solar system? 
 //     Mercury
 
-// What had a gravitational pull so strong that even light cannot escape it?
+// What has a gravitational pull so strong that even light cannot escape it?
 //     Black Hole
 
 // What is the Earth's Moon commonly referred to? Hint - It's Latin for Moon and the name of one of my pups.
