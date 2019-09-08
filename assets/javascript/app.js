92d91949783cfc4f4dfcba1a9caea08eca3703cb
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-09-07 22:51:01 -0400

    took my best shot at pseudo coding.  Questions, answers and images saved.  Some HTML done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,58 @@
 alert("TESTING");
+
+//                     global variables
+// ------------------------------------------------------------
+
+var triviaQuestions = {
+    question: "What type of galaxy is the most common in the universe?",
+    answer: "Elliptical Gallaxies"
+};  console.log("QUESTION " + triviaQuestions.question);
+
+
+
+
+
+//                  Questions/Answers/Images
+// --------------------------------------------------------------                        
+// What type of galaxy is the Milky Way galaxy?
+//     Spiral
+// assets/images/Milky_Way_Galaxy.jpg
+
+// What is the largest planet in our solar system? (FunFact - this planet also happens to be my favorite!)
+//     Jupiter
+
+// What is the smallest planet in our solar system? 
+//     Mercury
+
+// What had a gravitational pull so strong that even light cannot escape it?
+//     Black Hole
+
+// What is the Earth's Moon commonly referred to? Hint - It's Latin for Moon and the name of one of my pups.
+//     Luna
+
+// How much time does it take for the Sun's Rays to reach Earth?
+//     8 minutes
+
+// Which planet has approximately the same landmass as Earth?
+//     Mars
+
+// When was Pluto reclassified from a planet to a dwarf planet? (You're still a planet to me little buddy)
+//     2006
+
+// What are the most luminous objects in the known Universe?
+//     Quasars
+
+// What was the first planet discovered with the use of a telescope?
+//     Uranus 
+
+
+
+// click start to start Game
+// 20 second timer for each question
+// if answer is correct show photo with "correct"
+// if answer is incorrect show photo with "incorrect" and 
+//     correct answer
+// if time runs out show "time is up" and correct answer
+// at end of game show number of correct answers, incorrect answers
+//     unanswered and Start Over button
+// Start over DOES NOT reload page, it only restarts game
